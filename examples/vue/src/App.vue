458da27e9e3edb6b8113ae6c2e28469a80d93e2f
Blockchain Zoo <blockchainzoo@BlockchainZoo.local>
2019-08-01 16:49:16 +0800

    vuejs example for zoobc-sdk-js

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <table>
+      <thead>
+        <th>Id</th>
+        <th>Previous Hash</th>
+        <th>Height</th>
+        <th>Timestamp</th>
+        <th>Version</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>a</td>
+          <td>b</td>
+          <td>c</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import zoobc from 'zoobc'
+
+export default {
+  name: 'app',
+  state: {
+    blocks: [],
+    error: ''
+  },
+
+  beforeCreate() {
+    this.listBlocks
+  },
+
+  listBlocks() {
+    zoobc.connection('http://18.139.3.139:7001')
+    zoobc.getBlocks(0, 5, 1)
+      .then(res => {
+        this.setState({ blocks: res.blocksList })
+      })
+      .catch(err => {
+        this.setState({ error: err })
+      })
+    }
+}
+
+</script>
