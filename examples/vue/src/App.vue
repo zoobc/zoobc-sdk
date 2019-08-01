6c10b3a848c9cb386b580e78902ab6cbc63baad4
Blockchain Zoo <blockchainzoo@BlockchainZoo.local>
2019-08-01 17:34:16 +0800

    vuejs example for zoobc-sdk-js

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="app">
-    <table>
-      <thead>
-        <th>Id</th>
-        <th>Previous Hash</th>
-        <th>Height</th>
-        <th>Timestamp</th>
-        <th>Version</th>
-      </thead>
-      <tbody>
-        <tr>
-          <td>a</td>
-          <td>b</td>
-          <td>c</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <table>
+    <thead>
+      <th>Id</th>
+      <th>Previous Hash</th>
+      <th>Height</th>
+      <th>Timestamp</th>
+      <th>Version</th>
+    </thead>
+    <tbody>
+      <tr v-for="block in blocks" :key="block.id">
+        <td>{{block.id}}</td>
+        <td>{{block.previousblockhash}}</td>
+        <td>{{block.height}}</td>
+        <td>{{block.timestamp}}</td>
+        <td>{{block.version}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
@@ -24,25 +24,33 @@ import zoobc from 'zoobc'
 
 export default {
   name: 'app',
-  state: {
-    blocks: [],
-    error: ''
-  },
-
-  beforeCreate() {
-    this.listBlocks
-  },
-
-  listBlocks() {
-    zoobc.connection('http://18.139.3.139:7001')
-    zoobc.getBlocks(0, 5, 1)
-      .then(res => {
-        this.setState({ blocks: res.blocksList })
-      })
-      .catch(err => {
-        this.setState({ error: err })
-      })
+  data () {
+    return {
+      blocks: [],
+      error: ''
     }
+  },
+
+  created() {
+    this.listBlocks()
+  },
+
+  methods: {
+    listBlocks() {
+      zoobc.connection('http://18.139.3.139:7001')
+      zoobc.getBlocks(0, 5, 1)
+        .then(res => {
+          this.blocks = res.blocksList
+        })
+        .catch(err => {
+          this.error = err
+        })
+      }
+  }
 }
 
 </script>
+
+<style scoped>
+  
+</style>
