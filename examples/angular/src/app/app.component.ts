466fdd0fa7033034f07d85cf7f44b960e7b4890b
KevinH2810 <kevinhadisutanto@gmail.com>
2019-08-01 14:21:53 +0800

    adding angular

---
+++ examples/angular/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import zoobc from 'zoobc';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: `./app.component.html`,
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  list = [];
+  title = 'angular app';
+
+  ngOnInit(): void {
+    this.getBlocks().then(data => {
+      this.list = data.blocksList;
+      console.log(this.list);
+    });
+  }
+
+  getBlocks() {
+    zoobc.connection('http://18.139.3.139:7001');
+    const a = zoobc.getBlocks(0, 5, 1);
+    return a;
+  }
+}
