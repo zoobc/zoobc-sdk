e6176ab9f4af3ff8aec800ff325b8b47b3b5250e
gedenata <igedetirtanata@gmail.com>
2019-08-05 11:09:26 +0800

    fix footer link menu

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
 
 class Footer extends React.Component {
@@ -37,93 +30,26 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Products</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              ZooBC Core
-            </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              ZooBC Wallet
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              ZooBC Explorer
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              White Paper
-            </a>
+            <a href={this.docUrl('doc1.html', this.props.language)}>ZooBC Core</a>
+            <a href={this.docUrl('doc2.html', this.props.language)}>ZooBC Wallet</a>
+            <a href={this.docUrl('doc3.html', this.props.language)}>ZooBC Explorer</a>
+            <a href={this.docUrl('doc3.html', this.props.language)}>White Paper</a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              Getting Started
-            </a>
-            <a
-              href="https://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Developer APIs
-            </a>
-            <a href="https://discordapp.com/">Research</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Roadmap
-            </a>
+            <a href={this.pageUrl('users.html', this.props.language)}>Getting Started</a>
+            <a href="#">Developer APIs</a>
+            <a href="#">Research</a>
+            <a href="#">Roadmap</a>
           </div>
           <div>
             <h5>Company</h5>
             <a href={`${this.props.config.baseUrl}blog`}>About Us</a>
-            <a href="https://github.com/">Contact Us</a>
-            <a href="https://github.com/">Terms of Service</a>
-            <a href="https://github.com/">Privacy Policy</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
-            </a>
-            {this.props.config.twitterUsername && (
-              <div className="social">
-                <a
-                  href={`https://twitter.com/${
-                    this.props.config.twitterUsername
-                  }`}
-                  className="twitter-follow-button">
-                  Follow @{this.props.config.twitterUsername}
-                </a>
-              </div>
-            )}
-            {this.props.config.facebookAppId && (
-              <div className="social">
-                <div
-                  className="fb-like"
-                  data-href={this.props.config.url}
-                  data-colorscheme="dark"
-                  data-layout="standard"
-                  data-share="true"
-                  data-width="225"
-                  data-show-faces="false"
-                />
-              </div>
-            )}
+            <a href="#">Contact Us</a>
+            <a href="#">Terms of Service</a>
+            <a href="#">Privacy Policy</a>
           </div>
         </section>
-
-        <a
-          href="https://opensource.facebook.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
