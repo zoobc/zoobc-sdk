0f4d489c8f64430fa3176c271aec19950f4ad870
bagasAgastya <bagasagastya19@gmail.com>
2020-05-19 16:59:38 +0800

    add recipient field

--- src/helper/wallet/MultiSignature.ts
+++ src/helper/wallet/MultiSignature.ts
@@ -7,12 +7,14 @@ export function toGetPendingList(res: MultisigPendingTxResponse) {
     const amount = readInt64(bytes, 121);
     const fee = readInt64(bytes, 109);
     const timestamp = readInt64(bytes, 5);
+    const recipient = bytes.slice(65, 109);
     return {
       amount: amount,
       blockheight: tx.blockheight,
       fee: fee,
       latest: tx.latest,
       senderaddress: tx.senderaddress,
+      recipientaddress: recipient.toString(),
       status: tx.status,
       timestamp: timestamp,
       transactionhash: tx.transactionhash,
--- types/helper/wallet/MultiSignature.d.ts
+++ types/helper/wallet/MultiSignature.d.ts
@@ -8,6 +8,7 @@ export declare function toGetPendingList(res: MultisigPendingTxResponse): {
         fee: number;
         latest: boolean;
         senderaddress: string;
+        recipientaddress: string;
         status: 0 | 1 | 2 | 3;
         timestamp: number;
         transactionhash: string | Uint8Array;
