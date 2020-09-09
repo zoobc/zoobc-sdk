d2018b9e0213319be76a2177d41c7da47d8003ee
gungdesurya <gungde.avenger@gmail.com>
2020-09-09 17:31:41 +0800

    shorting address/hash

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -64,6 +64,20 @@ export function ZBCAddressToBytes(address: string): Buffer {
   return buffer.slice(0, 32);
 }
 
+export function shortenHash(text = ''): string {
+  if (!text) return text;
+
+  const split = text.split('_');
+  const zoobcPrefix = split[0];
+  const head = split[1];
+  const tail = split[split.length - 1];
+
+  const truncateHead = head.slice(0, head.length - 4);
+  const truncateTail = tail.slice(tail.length - 4, tail.length);
+
+  return `${zoobcPrefix}_${truncateHead}...${truncateTail}`;
+}
+
 export function writeInt64(number: number | string, base?: number, endian?: any): Buffer {
   number = number.toString();
   const buffer = new Int64LE(number);
