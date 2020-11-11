6b337d7af170a5f7a02ec401997100a07028e2b2
Bagas Agastya <43229728+bagasAgastya@users.noreply.github.com>
2020-11-11 08:25:43 +0800

    [ENHANCEMENT] add txtype filter param on toZBCPendingTransactions helper #296

--- dist/zoobc-sdk.mjs
+++ dist/zoobc-sdk.mjs
@@ -49799,7 +49799,7 @@ function toUnconfirmTransactionNodeWallet(res) {
     }
     return result;
 }
-function toZBCPendingTransactions(res) {
+function toZBCPendingTransactions(res, transactionType) {
     let mempoolTx = res.mempooltransactionsList;
     let transactions = [];
     let transaction;
@@ -49847,6 +49847,7 @@ function toZBCPendingTransactions(res) {
         }
         transactions.push(transaction);
     }
+    transactions = transactions.filter(txs => txs.transactionType == transactionType);
     return transactions;
 }
 
--- types/helper/wallet/Mempool.d.ts
+++ types/helper/wallet/Mempool.d.ts
@@ -2,4 +2,4 @@ import { ZBCTransaction } from '../..';
 import { GetMempoolTransactionsResponse } from '../../../grpc/model/mempool_pb';
 export declare function toUnconfirmedSendMoneyWallet(res: GetMempoolTransactionsResponse.AsObject, ownAddress: string): any;
 export declare function toUnconfirmTransactionNodeWallet(res: GetMempoolTransactionsResponse.AsObject): any;
-export declare function toZBCPendingTransactions(res: GetMempoolTransactionsResponse.AsObject): ZBCTransaction[];
+export declare function toZBCPendingTransactions(res: GetMempoolTransactionsResponse.AsObject, transactionType: number): ZBCTransaction[];
