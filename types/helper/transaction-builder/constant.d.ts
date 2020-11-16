5dc3b576b63626b206b07eee7491102438b99aab
bagasAgastya <bagasagastya19@gmail.com>
2020-11-16 09:55:07 +0800

    remove types

--- types/helper/transaction-builder/constant.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-export declare const VERSION: Buffer;
-export declare const ADDRESS_LENGTH = 32;
-export declare const ADDRESS_WITH_TYPE = 36;
-export declare const POOWN_LENGTH = 136;
--- types/helper/wallet/Mempool.d.ts
+++
@@ -1,5 +0,0 @@
-import { ZBCTransaction, ZBCTransactions } from '../..';
-import { GetMempoolTransactionsResponse, MempoolTransaction } from '../../../grpc/model/mempool_pb';
-export declare function toUnconfirmTransactionNodeWallet(res: GetMempoolTransactionsResponse.AsObject): any;
-export declare function toZBCPendingTransactions(mempools: GetMempoolTransactionsResponse.AsObject): ZBCTransactions;
-export declare function toZBCPendingTransaction(mempool: MempoolTransaction.AsObject): ZBCTransaction;
