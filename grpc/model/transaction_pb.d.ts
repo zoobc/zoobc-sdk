dcedf55fde06a3b11282c64b69e8cb59762d66f0
Gung De Surya <gungde.avenger@gmail.com>
2021-01-29 14:02:29 +0800

    [ENHANCEMENT] add transaction type label on transactions list #319

--- grpc/model/transaction_pb.d.ts
+++ grpc/model/transaction_pb.d.ts
@@ -610,6 +610,12 @@ export class GetTransactionsRequest extends jspb.Message {
   getPagination(): model_pagination_pb.Pagination | undefined;
   setPagination(value?: model_pagination_pb.Pagination): void;
 
+  getFromblock(): number;
+  setFromblock(value: number): void;
+
+  getToblock(): number;
+  setToblock(value: number): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): GetTransactionsRequest.AsObject;
   static toObject(includeInstance: boolean, msg: GetTransactionsRequest): GetTransactionsRequest.AsObject;
@@ -628,6 +634,8 @@ export namespace GetTransactionsRequest {
     timestampend: string,
     transactiontype: number,
     pagination?: model_pagination_pb.Pagination.AsObject,
+    fromblock: number,
+    toblock: number,
   }
 }
 
