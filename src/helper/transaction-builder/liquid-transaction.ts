8eb92570c32993bda05b43bc50a77c0248c30690
eksant <eksant@gmail.com>
2021-03-02 12:29:15 +0800

    Fixing Bug Cannot find name EscrowTransactionInterface

--- src/helper/transaction-builder/liquid-transaction.ts
+++ src/helper/transaction-builder/liquid-transaction.ts
@@ -2,13 +2,12 @@
 // The Quasisoft Limited - Hong Kong licenses this file to you under MIT license.
 
 import { BIP32Interface } from 'bip32';
-import { AccountType } from '../../../grpc/model/accountType_pb';
 import { TransactionType } from '../../../grpc/model/transaction_pb';
 import { Address } from '../interfaces';
+import { EscrowTransactionInterface } from './send-money';
 import { writeInt64, writeInt32, ZBCAddressToBytes, addressToBytes, generateTransactionHash } from '../utils';
 import { VERSION } from './constant';
 import { addEscrowBytes } from './escrow-transaction';
-import { SendZBCInterface } from './send-money';
 
 const TRANSACTION_TYPE = writeInt32(TransactionType.LIQUIDPAYMENTTRANSACTION);
 
